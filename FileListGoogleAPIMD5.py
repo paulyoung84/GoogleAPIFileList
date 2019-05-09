14db46fd3cf1118f0efebf88e8854d002a86785b
paulyoung84 <paulyoung84@hotmail.co.uk>
2019-05-09 16:34:42 +0100

    Update FileListGoogleAPIMD5.py

--- FileListGoogleAPIMD5.py
+++ FileListGoogleAPIMD5.py
@@ -81,3 +81,145 @@ def get_credentials():
 
 
     Returns:
+
+        Credentials, the obtained credential.
+
+    """
+
+    home_dir = os.path.expanduser('~')
+
+    credential_dir = os.path.join(home_dir, '.credentials')
+
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+
+    credential_path = os.path.join(credential_dir,
+
+                                   'drive-python-quickstart.json')
+
+    store = Storage(credential_path)
+
+    credentials = store.get()
+
+    if not credentials or credentials.invalid:
+
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+
+        flow.user_agent = APPLICATION_NAME
+
+        if flags:
+
+            credentials = tools.run_flow(flow, store, flags)
+
+        else:  # Needed only for compatibility with Python 2.6
+
+            credentials = tools.run(flow, store)
+
+        print('Storing credentials to ' + credential_path)
+
+    return credentials
+
+
+def get_root_folder(): # get's folder list from original root folder
+
+    credentials = get_credentials()
+
+    http = credentials.authorize(httplib2.Http())
+
+    service = discovery.build('drive', 'v3', http=http)
+
+    results = service.files().list(q="mimeType = 'application/vnd.google-apps.folder' and '"+folder_id+"' in parents",
+
+        pageSize=1000, fields="nextPageToken, files(id, mimeType)").execute()
+
+    folders = results.get('files', [])
+
+    if not folders:
+
+        print('No folders found.')
+
+    else:
+
+        for folder in folders:
+            id = folder.get('id')
+            folder_list.append(id)
+
+
+def get_all_folders(folder_list): #creates list of all sub folder under root
+
+    for folder in folder_list:
+        additional_folders = []
+        credentials = get_credentials()
+
+        http = credentials.authorize(httplib2.Http())
+
+        service = discovery.build('drive', 'v3', http=http)
+        results = service.files().list(
+            q="mimeType = 'application/vnd.google-apps.folder' and '" +folder+ "' in parents",
+
+            pageSize=1000, fields="nextPageToken, files(id, mimeType)").execute()
+        items = results.get('files', [])
+
+        for item in items:
+            id = item.get('id')
+            additional_folders.append(id)
+        if not additional_folders:
+            pass
+        else:
+            all_folders.extend(additional_folders)
+            folder_list = additional_folders
+            get_all_folders(folder_list)
+
+def merge(): #merges sub folder list with full list
+    global full_list
+    full_list = all_folders + folder_list
+    full_list.append(folder_id)
+
+
+def get_file_list(): #runs over each folder generating file list, for files over 1000 uses nextpagetoken to run additional requests
+
+
+    for folder in full_list:
+        credentials = get_credentials()
+
+        http = credentials.authorize(httplib2.Http())
+
+        service = discovery.build('drive', 'v3', http=http)
+
+        page_token = None
+        while True:
+            results = service.files().list(
+                q="'" + folder + "' in parents",
+
+                pageSize=1000, fields="nextPageToken, files(name, md5Checksum, size, id, parents)", pageToken=page_token).execute()
+
+            items = results.get('files', [])
+            for item in items:
+                name = item['name']
+
+                checksum = item.get('md5Checksum', 'no checksum')
+
+                size = item.get('size', '-')
+
+                id = item.get('id')
+
+                parents = item.get('parents')
+
+                file_list.append([name, checksum, size, id, parents])
+
+            page_token = results.get('nextPageToken', None)
+            if page_token is None:
+                break
+
+    files = pd.DataFrame(file_list,columns=['File_Name','Checksum','Size','ID', 'Parents'])
+    print(files)
+    files.to_csv('H:/test.csv')
+
+
+if __name__ == '__main__':
+    print('Collecting folder id list')
+    get_root_folder()
+    get_all_folders(folder_list)
+    merge()
+    print('Generating file metadata list')
+    get_file_list()
